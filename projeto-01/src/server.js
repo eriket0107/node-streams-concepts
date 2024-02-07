12d40f9b4f5bcb590dab660f0feb397e6197c757
Erik <ol.erik0107@gmail.com>
2024-02-07 06:26:48 -0300

    chore: removing comments

--- projeto-01/src/server.js
+++ projeto-01/src/server.js
@@ -1,31 +1,6 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
 
-// - Criar usuários
-// - Listagem usuários
-// - Edição de usuários
-// - Remoção de usuários
-
-// - HTTP
-//   - Método HTTP
-//   - URL
-
-// GET, POST, PUT, PATCH, DELETE
-
-// GET => Buscar um recurso do back-end
-// POST => Criar um recurso no back-end
-// PUT => Atualizar um recurso no back-end
-// PATCH => Atualizar uma informação específica de um recurso no back-end
-// DELETE => Deletar um recurso do back-end
-
-// GET /users => Buscando usuários no banc-end
-// POST /users => Criar um usuário no back-end
-
-// Stateful - Stateless
-
-// Cabeçalhos (Requisição/resposta) => Metadados
-
-// HTTP Status Code
 
 const users = []
 
